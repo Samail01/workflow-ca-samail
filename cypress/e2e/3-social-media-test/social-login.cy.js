cdfa52199e2c90e2a3b8cb16d0f65f25abb952c9
Samail01 <sapug.2001@hotmail.com>
2023-11-13 11:34:07 +0100

    cypress finished

--- cypress/e2e/3-social-media-test/social-login.cy.js
+++ cypress/e2e/3-social-media-test/social-login.cy.js
@@ -11,5 +11,25 @@ describe('example social media client', () => {
     cy.get('#loginPassword').type(Cypress.env('USER_PASSWORD'));
 
     cy.get('.btn-success').contains('Login').click();
+    cy.url().should('include', '?view=profile&name=Samail');
+    cy.wait(3000);
+
+    //for Logout
+    cy.get(
+      'body > header > div > div > div > button.btn.btn-outline-warning.me-2',
+    )
+      .contains('Logout')
+      .click();
+    cy.wait(1000);
+
+    //For wrong Login
+    cy.get(
+      '#registerForm > div.modal-footer > button.btn.btn-outline-success',
+    ).click();
+    cy.wait(1000);
+
+    cy.get('#loginEmail').type('wrong@stud.noroff.no');
+    cy.get('#loginPassword').type('wrongpass123');
+    cy.get('.btn-success').contains('Login').click();
   });
 });
